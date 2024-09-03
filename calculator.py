6571c1b664ed26101e9697411e8640f46b297b0d
Baraah <57688502+BaraahAbdullah@users.noreply.github.com>
2024-09-03 23:48:06 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,26 +1,43 @@
 def add(x, y):
+     """Return the sum of x and y"""
     return x + y
 
 
 def subtract(x, y):
-    pass
+    """Return the difference between x and y."""
+    return x - y
 
 
 def divide(x, y):
-    pass
+     """Return the result of dividing x by y.
+    If y is 0, return a warning message instead.
+    """
+    if y == 0:
+        return "Invalid value for denominator, can't divide by 0!"
+    return x / y
 
 
 def multiply(x, y):
-    pass
+     """Return the product of x and y."""
+    return x * y
 
 
 def square(x):
-    pass
+     """Return the square of x."""
+    return x ** 2
 
 
 def power(x, y):
-    pass
+     """Return x raised to the power of y."""
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    """Return the square root of x.
+    If x is negative, return a warning message instead.
+    """
+    if x < 0:
+        return "Invalid value for square root, can't take the square root of a negative number!"
+    return math.sqrt(x)
+
+
